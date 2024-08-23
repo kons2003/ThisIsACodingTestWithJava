11d2a797ac3428ac753d9dd4cefdbc8a92b70f03
kons2003 <mj030313@naver.com>
2024-08-23 11:07:03 +0900

    큰 수의 법칙

--- src/ch3/Prac3_2.java
+++ src/ch3/Prac3_2.java
@@ -5,10 +5,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
-public class Ch3_2 {
+public class Prac3_2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
